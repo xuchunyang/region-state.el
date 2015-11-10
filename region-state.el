9d42868aa02ae7f01789adeeb88d790d626fad52
Chunyang Xu <xuchunyang56@gmail.com>
2015-11-10 14:58:58 +0800

    Add Version keywork for installing as package
    Reported when M-x package-install-from-buffer

--- region-state.el
+++ region-state.el
@@ -4,6 +4,7 @@
 
 ;; Author: Chunyang Xu <xuchunyang56@gmail.com>
 ;; Keywords: convenience
+;; Version: 0.1
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
